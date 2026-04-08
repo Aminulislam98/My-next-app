560792977a00669fe6105bc3fa4f802d3285f80b
Aminul islam <aminurrohman98@gmail.com>
2026-04-08 22:09:28 +0100

    made about , contact and product NavLink and using href and next js path displayed data by looping

--- src/app/product/page.jsx
+++ src/app/product/page.jsx
@@ -1,9 +1,35 @@
-import React from "react";
-
 const Product = () => {
+  const data = [
+    {
+      id: 1,
+      name: "Aminur",
+      role: "Developer",
+    },
+    {
+      id: 2,
+      name: "John",
+      role: "Designer",
+    },
+    {
+      id: 3,
+      name: "Sara",
+      role: "Manager",
+    },
+    {
+      id: 4,
+      name: "Alex",
+      role: "Tester",
+    },
+  ];
   return (
     <div>
       <h1>This is Product Section </h1>
+      {data.map((d) => (
+        <div key={d.id}>
+          <h1>{d.name}</h1>
+          <p>{d.role}</p>
+        </div>
+      ))}
     </div>
   );
 };
