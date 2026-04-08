a55a6525083c4cd2083e63df55478b3acc3857c4
Aminul islam <aminurrohman98@gmail.com>
2026-04-08 21:52:22 +0100

    creating jsx component inside of the app ,  change path to check

---
+++ src/app/about/aboutMe/page.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const AboutMe = () => {
+  return (
+    <div>
+      <h1>Hi! This is Aminul islam</h1>
+    </div>
+  );
+};
+
+export default AboutMe;
---
+++ src/app/about/page.jsx
@@ -0,0 +1,10 @@
+const About = () => {
+  return (
+    <div>
+      <h1>This is about section</h1>
+      <p>Explore Next.js FrameWork</p>
+    </div>
+  );
+};
+
+export default About;
